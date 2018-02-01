b16ecce400a4c83847e0c23ddbd52096c56f50d1
aeciolevy <aeciolevy@gmail.com>
2018-02-01 09:25:48 -0800

    Redux setup complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+/* eslint react/jsx-filename-extension: 0, react/prefer-stateless-function: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
